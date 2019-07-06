b978b4e2ba62ff55ca5f0a44e9520d7a953f0f4b
Anton Strogonoff <anton@strogonoff.name>
2019-07-06 18:53:38 +0800

    feat: Add more message types

--- src/renderer/app/issues/models/messages.ts
+++ src/renderer/app/issues/models/messages.ts
@@ -2,19 +2,43 @@ import { PublicationID } from 'renderer/app/lists/models';
 import { ITURecVersion } from 'renderer/app/recommendations/models';
 
 
+interface AmendmentTarget {
+  publication: PublicationID,
+  position_on: string,
+}
+
 export interface ApprovedRecommendationsMessage {
   type: "approved_recommendations",
   items: { [code: string]: ITURecVersion },
 }
-
 export interface RunningAnnexesMessage {
   type: "running_annexes",
   extra_links: PublicationID[],
 }
+export interface AmendmentMessage {
+  type: "amendment",
+  target: AmendmentTarget,
+  contents: any,
+}
+export interface CustomMessage {
+  type: "custom",
+  contents: any,
+}
+export interface ServiceRestrictionsMessage {
+  type: "service_restrictions",
+  items: { country: string, ob: number, page: number }[],
+}
+export interface CallbackProceduresMessage {
+  type: "callback_procedures",
+}
 
 export type Message =
   ApprovedRecommendationsMessage |
-  RunningAnnexesMessage;
+  RunningAnnexesMessage |
+  ServiceRestrictionsMessage |
+  CallbackProceduresMessage |
+  AmendmentMessage |
+  CustomMessage;
 
 export type MessageType = Message["type"]
 
