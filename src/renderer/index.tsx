76327ac733bdf24cc6e0eefa7a52933273fd7e44
Anton Strogonoff <anton@strogonoff.name>
2019-07-07 13:31:51 +0800

    chore: UI adjustments

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -5,24 +5,30 @@ import { initStorage } from './app/storage';
 import '!style-loader!css-loader!@blueprintjs/datetime/lib/css/blueprint-datetime.css';
 import '!style-loader!css-loader!@blueprintjs/core/lib/css/blueprint.css';
 import '!style-loader!css-loader!./normalize.css';
+import * as styles from './styles.scss';
 
 
 async function initWindow() {
   const searchParams = new URLSearchParams(window.location.search);
   const storage = await initStorage();
 
+  // Electron Webpack skeleton guarantees that #app exists in index.html
+  const app = document.getElementById('app') as HTMLElement;
+
+  app.classList.add(styles.app);
+
   if (searchParams.get('c') === 'issueScheduler') { 
     ReactDOM.render(
       <IssueScheduler
         storage={storage} />,
-      document.getElementById('app'));
+      app);
 
   } else if (searchParams.get('c') === 'issueEditor') {
     ReactDOM.render(
       <IssueEditor
         storage={storage}
         issueId={searchParams.get('issueId') || ''} />,
-      document.getElementById('app'));
+      app);
   }
 }
 initWindow();
