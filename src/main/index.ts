8cb5429e32a22d3b0cea7f09cffb9e0d171d7b91
Anton Strogonoff <anton@strogonoff.name>
2019-09-05 18:44:48 +0800

    refactor: Move out window-related helpers (#4)

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,96 +1,20 @@
-import { app, Menu, ipcMain, BrowserWindow } from 'electron';
-import { createWindow } from './window';
+import { app, Menu, ipcMain } from 'electron';
+
 import { getMenu } from './menu';
+import { makeEndpoint, makeWriteOnlyEndpoint } from './api';
+import { openWindow, getWindowByTitle, windows } from './window';
 import { initRepo, initStorage, GitController, Workspace, Storage } from './storage';
+
 import { reviveJsonValue } from './storage/api';
 import { QuerySet, sortIntegerAscending, sortIntegerDescending } from './storage/query';
 import { OBIssue } from './issues/models';
-import { makeEndpoint, makeWriteOnlyEndpoint } from './api';
-
-
-/* Generic helpers
-   TODO (#4): Move into the framework */
-
-
-// Ensure only one instance of the app can run at a time on given user’s machine
-const gotTheLock = app.requestSingleInstanceLock();
-if (!gotTheLock) { app.exit(0); }
-
-
-/* This window-controlling helpers may be moved out to a separate module,
-   and/or encapsulated in a class,
-   as long as windows don’t get accidentally garbage-collected. */
-
-// Keeps track of windows and ensures (?) they do not get garbage collected
-var windows: BrowserWindow[] = [];
-
-// Allows to locate window ID by label
-var windowsByTitle: { [title: string]: BrowserWindow } = {};
-
-function windowIsOpen(title: string): boolean {
-  return windowsByTitle[title] !== undefined;
-}
-
-// Iterate over array of windows and try accessing window ID.
-// If it throws, window was closed and we remove it from the array.
-// Supposed to be run after any window is closed.
-function cleanUpWindows() {
-  var deletedWindows: number[] = [];
-  for (const [idx, win] of windows.entries()) {
-    // When accessing the id attribute of a closed window,
-    // it’ll throw. We’ll mark its index for deletion then.
-    try {
-      win.id;
-    } catch (e) {
-      deletedWindows.push(idx - deletedWindows.length);
-    }
-  }
-  for (const idx of deletedWindows) {
-    windows.splice(idx, 1);
-  }
-}
-
-interface WindowMakerParams {
-  title: string,
-  component: string,
-  componentParams?: string,
-  dimensions?: { minWidth?: number, width?: number, height?: number },
-  frameless?: boolean,
-  winParams?: any,
-}
-type WindowMaker = (props: WindowMakerParams) => BrowserWindow;
-const _createWindow: WindowMaker = ({ title, component, componentParams, dimensions, frameless, winParams }) => {
-
-  if (windowIsOpen(title)) {
-    windowsByTitle[title].focus();
-    return windowsByTitle[title];
-  }
-
-  const _framelessOpts = {
-    frame: process.platform === 'darwin' ? true : false,
-    titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : undefined,
-  };
-  const _winParams = {
-    width: (dimensions || {}).width,
-    height: (dimensions || {}).height,
-    ...(frameless === true ? _framelessOpts : {}),
-    ...winParams,
-  };
-  const params = `c=${component}&${componentParams ? componentParams : ''}`;
-  const window = createWindow(title, params, _winParams);
-
-  windows.push(window);
-  windowsByTitle[title] = window;
-  window.on('closed', () => { delete windowsByTitle[title]; cleanUpWindows(); });
-
-  return window;
-}
-
-
-/* App-specific initialization */
 
 
 const APP_TITLE = "ITU OB editor";
+
+
+// Ensure only one instance of the app can run at a time on given user’s machine
+if (!app.requestSingleInstanceLock()) { app.exit(0); }
 
 
 Promise.all([ initRepo(), initStorage(), app.whenReady() ]).then((...args) => {
@@ -172,8 +96,9 @@ Promise.all([ initRepo(), initStorage(), app.whenReady() ]).then((...args) => {
   });
 
   ipcMain.on('scheduled-new-issue', (event: any) => {
-    if (windowsByTitle[APP_TITLE]) {
-      windowsByTitle[APP_TITLE].webContents.send('update-current-issue');
+    const homeWindow = getWindowByTitle(APP_TITLE);
+    if (homeWindow !== undefined) {
+      homeWindow.webContents.send('update-current-issue');
     }
   });
 
@@ -203,7 +128,7 @@ Promise.all([ initRepo(), initStorage(), app.whenReady() ]).then((...args) => {
 
 
 function openHomeScreen() {
-  _createWindow({
+  openWindow({
     component: 'home',
     title: APP_TITLE,
     dimensions: { width: 400, height: 400, },
@@ -212,7 +137,7 @@ function openHomeScreen() {
 }
 
 function openIssueScheduler() {
-  _createWindow({
+  openWindow({
     component: 'issueScheduler',
     title: 'Issue Scheduler',
     frameless: true,
@@ -221,7 +146,7 @@ function openIssueScheduler() {
 }
 
 function openDataSynchronizer() {
-  _createWindow({
+  openWindow({
     component: 'dataSynchronizer',
     title: 'Data Synchronizer',
     dimensions: { width: 400, minWidth: 380, height: 400 },
@@ -229,7 +154,7 @@ function openDataSynchronizer() {
 }
 
 function openIssueEditor(issueId: string) {
-  _createWindow({
+  openWindow({
     component: 'issueEditor',
     title: `Issue ${issueId}`,
     componentParams: `issueId=${issueId}`,
--- src/main/window.ts
+++ src/main/window.ts
@@ -2,9 +2,91 @@ import * as path from 'path'
 import { format as formatUrl } from 'url';
 import { BrowserWindow } from 'electron';
 
+
+/* Window-related helpers
+   TODO (#4): Move into the framework */
+
+
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
-export function createWindow(title: string, params: string, winParams: any) {
+// Keeps track of windows and ensures (?) they do not get garbage collected
+export var windows: BrowserWindow[] = [];
+
+// Allows to locate window ID by label
+var windowsByTitle: { [title: string]: BrowserWindow } = {};
+
+
+// Open new window, or focus if one with the same title already exists
+interface WindowOpenerParams {
+  title: string,
+  component: string,
+  componentParams?: string,
+  dimensions?: { minWidth?: number, width?: number, height?: number },
+  frameless?: boolean,
+  winParams?: any,
+}
+type WindowOpener = (props: WindowOpenerParams) => BrowserWindow;
+export const openWindow: WindowOpener = ({
+    title,
+    component, componentParams,
+    dimensions, frameless,
+    winParams}) => {
+
+  const _existingWindow = getWindowByTitle(title);
+  if (_existingWindow !== undefined) {
+    _existingWindow.focus();
+    return _existingWindow;
+  }
+
+  const _framelessOpts = {
+    frame: process.platform === 'darwin' ? true : false,
+    titleBarStyle: process.platform === 'darwin' ? 'hiddenInset' : undefined,
+  };
+
+  const _winParams = {
+    width: (dimensions || {}).width,
+    height: (dimensions || {}).height,
+    ...(frameless === true ? _framelessOpts : {}),
+    ...winParams,
+  };
+
+  const params = `c=${component}&${componentParams ? componentParams : ''}`;
+  const window = createWindow(title, params, _winParams);
+
+  windows.push(window);
+  windowsByTitle[title] = window;
+  window.on('closed', () => { delete windowsByTitle[title]; cleanUpWindows(); });
+
+  return window;
+}
+
+
+export function getWindowByTitle(title: string): BrowserWindow | undefined {
+  return windowsByTitle[title];
+}
+
+
+// Iterate over array of windows and try accessing window ID.
+// If it throws, window was closed and we remove it from the array.
+// Supposed to be run after any window is closed
+function cleanUpWindows() {
+  var deletedWindows: number[] = [];
+  for (const [idx, win] of windows.entries()) {
+    // When accessing the id attribute of a closed window,
+    // it’ll throw. We’ll mark its index for deletion then.
+    try {
+      win.id;
+    } catch (e) {
+      deletedWindows.push(idx - deletedWindows.length);
+    }
+  }
+  for (const idx of deletedWindows) {
+    windows.splice(idx, 1);
+  }
+}
+
+
+function createWindow(title: string, params: string, winParams: any): BrowserWindow {
   const window = new BrowserWindow({
     webPreferences: {nodeIntegration: true},
     title: title,
