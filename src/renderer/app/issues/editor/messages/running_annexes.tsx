1f9c3a3a9dd83069feb0aee74c05484383aa4668
Anton Strogonoff <anton@strogonoff.name>
2019-09-11 03:26:05 +0800

    feat: Allow adding extra annexed lists (fix #26)

--- src/renderer/app/issues/editor/messages/running_annexes.tsx
+++ src/renderer/app/issues/editor/messages/running_annexes.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Tabs, Tab } from '@blueprintjs/core';
-import { SimpleEditableCard } from 'renderer/app/widgets/editable-card-list';
+import { Tabs, Tab, Button, Label, InputGroup } from '@blueprintjs/core';
+import { AddCardTrigger, SimpleEditableCard } from 'renderer/app/widgets/editable-card-list';
 import * as widgetStyles from 'renderer/app/widgets/styles.scss';
 
 import { RunningAnnexesMessage } from 'main/issues/messages/running_annexes';
 
 import { DateStamp } from 'renderer/app/dates';
 import { RunningAnnex, getRunningAnnexesForIssue } from 'renderer/app/issues/running-annexes';
-import { MessageEditorProps } from '../message-editor';
+import { MessageEditorProps, MessageEditorDialog } from '../message-editor';
 import * as styles from '../styles.scss';
 
 
@@ -67,17 +67,87 @@ const RunningAnnexes: React.FC<{ annexes: RunningAnnex[] }> = function ({ annexe
 };
 
 const ExtraPublications: React.FC<{ pubIds: string[], updateIds: (newIds: string[]) => void }> = function ({ pubIds, updateIds }) {
+  const [extraItemDialogState, toggleExtraItemDialogState] = useState(false);
+  const [activeItemIndex, setActiveItemIndex] = useState(0);
+
   return (
     <>
-      {pubIds.map((pubId) => (
-        <SimpleEditableCard
-            key={pubId}
-            onDelete={() => {
-              updateIds(pubIds.filter(id => id !== pubId));
-            }}>
-          {pubId}
-        </SimpleEditableCard>
+      <AddCardTrigger key="addFirst" onClick={() => {
+        setActiveItemIndex(0);
+        toggleExtraItemDialogState(true)
+      }} />
+
+      {pubIds.map((pubId: string, idx: number) => (
+        <>
+          <SimpleEditableCard
+              key="publication"
+              onDelete={() => {
+                updateIds(pubIds.filter(id => id !== pubId));
+              }}>
+            <strong>{pubId}</strong>
+          </SimpleEditableCard>
+
+          <AddCardTrigger key="addNewAfter" onClick={() => {
+            setActiveItemIndex(idx + 1);
+            toggleExtraItemDialogState(true)
+          }} />
+        </>
       ))}
+
+      {extraItemDialogState === true 
+        ? <AddExtraAnnexedListDialog
+            key="addNewDialog"
+            isOpen={true}
+            onClose={() => toggleExtraItemDialogState(false)}
+            onSave={(code) => {
+              var newIds = [...pubIds];
+              newIds.splice(activeItemIndex, 0, code);
+              updateIds(newIds);
+              toggleExtraItemDialogState(false);
+            }}
+          />
+        : ''}
     </>
+  );
+};
+
+
+interface AddExtraAnnexedListDialogProps {
+  isOpen: boolean,
+  onSave: (pubCode: string) => void,
+  onClose: () => void,
+}
+const AddExtraAnnexedListDialog: React.FC<AddExtraAnnexedListDialogProps> = function ({ isOpen, onSave, onClose }) {
+  const [code, setCode] = useState('');
+
+  function _onSave() {
+    onSave(code);
+    onClose();
+  }
+
+  return (
+    <MessageEditorDialog
+        title="Add extra annexed publication"
+        isOpen={isOpen}
+        onClose={onClose}
+        saveButton={
+          <Button
+            intent="primary"
+            disabled={code === ''}
+            onClick={_onSave}>Add publication</Button>
+        }>
+      <Label key="code">
+        Publication code
+        <InputGroup
+          value={code}
+          placeholder="E.g., BUREAUFAX"
+          type="text"
+          large={true}
+          onChange={(evt: React.FormEvent<HTMLElement>) => {
+            setCode((evt.target as HTMLInputElement).value as string);
+          }}
+        />
+      </Label>
+    </MessageEditorDialog>
   );
 };
