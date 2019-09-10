a67b0593e281f84b0d7900b09107cfc209fa72f3
Anton Strogonoff <anton@strogonoff.name>
2019-09-11 02:25:01 +0800

    feat: Approved recommendations editor (fix #18)

--- src/renderer/app/issues/editor/messages/approved_recommendations.tsx
+++ src/renderer/app/issues/editor/messages/approved_recommendations.tsx
@@ -1,57 +1,116 @@
-import React, { useState } from 'react';
-import { Card, Drawer, H5, Position, Navbar, NavbarGroup, Button, InputGroup } from '@blueprintjs/core';
+import * as moment from 'moment';
+import React, { useState, useEffect } from 'react';
+
+import { Button, Label, InputGroup } from '@blueprintjs/core';
+import { AddCardTrigger, SimpleEditableCard } from 'renderer/app/widgets/editable-card-list';
+import * as widgetStyles from 'renderer/app/widgets/styles.scss';
+
+import { ITURecCode, ITURecVersion } from 'main/recommendations/models';
 
 import { ApprovedRecommendationsMessage } from 'main/issues/messages/approved_recommendations';
 
-import { MessageEditorProps } from '../message-editor';
+import { MessageEditorProps, MessageEditorDialog } from '../message-editor';
 
 
 export const ApprovedRecommendationsEditor: React.FC<MessageEditorProps> = function (props) {
-  const [ addDialogStatus, updateAddDialogStatus ] = useState(false);
-  const [ newRecCode, updateNewRecCode ] = useState('');
-  const [ newRecVersion, updateNewRecVersion ] = useState('');
+  const [newRecDialogStatus, toggleNewRecDialogStatus] = useState(false);
+  const [recs, updateRecs] = useState(
+    (props.message as ApprovedRecommendationsMessage).items);
 
-  const recommendations = (props.message as ApprovedRecommendationsMessage).items;
+  useEffect(() => {
+    props.onChange({ items: recs });
+  }, [JSON.stringify(recs)]);
 
-  function onClose() {
-    if (newRecCode != '' && newRecVersion != '') {
-      var newRecs = recommendations;
-      newRecs[newRecCode] = newRecVersion;
-      props.onChange({ items: newRecs });
-    }
-    updateAddDialogStatus(false);
-    updateNewRecCode('');
-    updateNewRecVersion('');
+  return (
+    <>
+      <h2 key="paneHeader" className={widgetStyles.paneHeader}>Approved Recommendations</h2>
+
+      <AddCardTrigger key="addNew" onClick={() => {
+        toggleNewRecDialogStatus(true);
+      }} />
+
+      <>
+        {Object.entries(recs).map(([code, version]: [string, string]) => {
+          const pub = props.workspace.recommendations[code];
+          const title = pub ? pub.title.en : '';
+          return (
+            <SimpleEditableCard
+              key={code}
+              onDelete={() => {
+                updateRecs((recs) => { const { [code]: _, ...newRecs } = recs; return newRecs; });
+              }}>
+              <strong>{`${code} (${moment(version).format('YYYY-MM')})`}</strong>
+              <em>{title}</em>
+            </SimpleEditableCard>
+          );
+        })}
+      </>
+
+      {newRecDialogStatus === true 
+        ? <AddApprovedRecDialog
+            key="addRec"
+            isOpen={true}
+            onClose={() => toggleNewRecDialogStatus(false)}
+            onSave={(code, version) => {
+              updateRecs(recs => { recs[code] = version; return recs; });
+              toggleNewRecDialogStatus(false);
+            }}
+          />
+        : ''}
+    </>
+  );
+};
+
+
+interface AddApprovedRecDialogProps {
+  isOpen: boolean,
+  onSave: (code: ITURecCode, version: ITURecVersion) => void,
+  onClose: () => void,
+}
+const AddApprovedRecDialog: React.FC<AddApprovedRecDialogProps> = function ({ isOpen, onSave, onClose }) {
+  const [code, setCode] = useState('');
+  const [version, setVersion] = useState('');
+
+  function _onSave() {
+    onSave(code, version);
+    onClose();
   }
 
   return (
-    <React.Fragment>
-      <Navbar>
-        <NavbarGroup>
-          <Button onClick={() => updateAddDialogStatus(true)}>Add new…</Button>
-        </NavbarGroup>
-      </Navbar>
-      <Card>
-        {Object.entries(recommendations).map(([code, version]: [string, string]) => {
-          const pub = props.workspace.recommendations[code];
-          const title = pub ? pub.title.en : '';
-          return <Card key={code}><H5>{code} ({version})</H5>{title}</Card>
-        })}
-      </Card>
-      <Drawer usePortal={false} position={Position.TOP} isOpen={addDialogStatus} onClose={onClose}>
+    <MessageEditorDialog
+        title="Add approved recommendation"
+        isOpen={isOpen}
+        onClose={onClose}
+        saveButton={
+          <Button
+            intent="primary"
+            disabled={code === '' || version == ''}
+            onClick={_onSave}>Add recommendation</Button>
+        }>
+      <Label key="code">
+        Code
         <InputGroup
-          value={newRecCode}
-          placeholder="Code"
-          onChange={(evt: React.FormEvent<HTMLElement>) =>
-            updateNewRecCode((evt.target as HTMLInputElement).value as string)}
+          value={code}
+          placeholder="E.g., “L.1015” or “Q.850 (2018) Amendment 1” (no quotes!)"
+          type="text"
+          large={true}
+          onChange={(evt: React.FormEvent<HTMLElement>) => {
+            setCode((evt.target as HTMLInputElement).value as string);
+          }}
         />
+      </Label>
+      <Label key="version">
+        Version
         <InputGroup
-          value={newRecVersion}
-          placeholder="Version"
-          onChange={(evt: React.FormEvent<HTMLElement>) =>
-            updateNewRecVersion((evt.target as HTMLInputElement).value as string)}
+          value={version}
+          placeholder="E.g., 2019-04"
+          type="text"
+          large={true}
+          onChange={(evt: React.FormEvent<HTMLElement>) => {
+            setVersion((evt.target as HTMLInputElement).value as string);
+          }}
         />
-      </Drawer>
-    </React.Fragment>
+      </Label>
+    </MessageEditorDialog>
   );
 };
