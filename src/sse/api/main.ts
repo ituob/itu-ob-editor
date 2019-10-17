7bb9cb5cbb2b3b8d006d50d8a8132ef2a1dc5690
Anton Strogonoff <anton@strogonoff.name>
2019-10-17 21:03:49 +0800

    refactor(sse/api): Expand/type makeEndpoint a bit

--- src/sse/api/main.ts
+++ src/sse/api/main.ts
@@ -4,14 +4,26 @@ import { openWindow } from 'sse/main/window';
 import { reviveJsonValue } from './utils';
 
 
-type Saver<T> = (newObj: T) => Promise<void>;
-type Fetcher<T> = (params: any) => Promise<T>;
+type Saver<I> = (input: I) => Promise<void>;
+type Fetcher<O> = (params: any) => Promise<O>;
 
 export function makeEndpoint<T>(name: string, fetcher: Fetcher<T>, saver?: Saver<{ newData: T, notify?: string[] }>) {
   ipcMain.on(`request-workspace-${name}`, async (evt: any, params?: string, newObj?: string) => {
+    // TODO: Electron should be handling JSON [de]serialization for us, refactor!
     const parsedParams: any = JSON.parse(params || '{}', reviveJsonValue);
-    if (saver && newObj) { await saver(JSON.parse(newObj, reviveJsonValue)); }
-    evt.reply(`workspace-${name}`, JSON.stringify(await fetcher(parsedParams)));
+    let result: any;
+
+    if (saver && newObj) {
+      await saver(JSON.parse(newObj, reviveJsonValue));
+    }
+
+    if (fetcher) {
+      result = await fetcher(parsedParams);
+    } else {
+      result = { success: true };
+    }
+
+    evt.reply(`workspace-${name}`, JSON.stringify(result));
   });
 }
 
