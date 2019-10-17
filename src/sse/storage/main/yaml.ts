b67b557362dc1f0d67a71cf3b85d075c9ffbe6a7
Anton Strogonoff <anton@strogonoff.name>
2019-10-17 21:02:34 +0800

    feat(storage/yaml): Storing undefined removes file

--- src/sse/storage/main/yaml.ts
+++ src/sse/storage/main/yaml.ts
@@ -10,41 +10,52 @@ export class YAMLStorage {
     return yaml.load(data, { schema: SCHEMA });
   }
 
-  public async store(filePath: string, data: any): Promise<any> {
-    // Merge new data into old data; this way if some YAML properties
-    // are not supported we will not lose them after the update.
+  private async loadIfExists(filePath: string): Promise<any> {
     let fileExists: boolean;
     let oldData: any;
+
     try {
       fileExists = (await this.fs.stat(filePath)).isFile() === true;
     } catch (e) {
       fileExists = false;
     }
+
     if (fileExists) {
       oldData = await this.load(filePath);
     } else {
       oldData = {};
     }
-    const newData: any = Object.assign({}, oldData, data);
 
-    // console.debug(`Dumping contents for ${filePath} from ${data}`);
-    // console.debug(oldData);
+    return oldData;
+  }
 
-    const newContents: string = yaml.dump(newData, {
-      schema: SCHEMA,
-      noRefs: true,
-      noCompatMode: true,
-    });
+  public async store(filePath: string, data: any): Promise<any> {
+    if (data !== undefined) {
+      // Merge new data into old data; this way if some YAML properties
+      // are not supported we will not lose them after the update.
+      const newData: any = Object.assign(await this.loadIfExists(filePath), data);
 
-    // console.debug(`Writing to ${filePath}, file exists: ${fileExists}`);
+      // console.debug(`Dumping contents for ${filePath} from ${data}`);
+      // console.debug(oldData);
 
-    // if (fileExists) {
-    //   const oldContents: string = await this.fs.readFile(filePath, { encoding: 'utf8' });
-    //   console.debug(`Replacing contents of ${filePath}`, oldContents, newContents);
-    // }
+      const newContents: string = yaml.dump(newData, {
+        schema: SCHEMA,
+        noRefs: true,
+        noCompatMode: true,
+      });
 
-    await this.fs.writeFile(filePath, newContents, { encoding: 'utf8' });
-    return data;
+      // console.debug(`Writing to ${filePath}, file exists: ${fileExists}`);
+
+      // if (fileExists) {
+      //   const oldContents: string = await this.fs.readFile(filePath, { encoding: 'utf8' });
+      //   console.debug(`Replacing contents of ${filePath}`, oldContents, newContents);
+      // }
+
+      await this.fs.writeFile(filePath, newContents, { encoding: 'utf8' });
+      return data;
+    } else {
+      await this.fs.remove(filePath);
+    }
   }
 }
 
