0daa80994bf07dd3cb929d6e854656dcd4d06784
Anton Strogonoff <anton@strogonoff.name>
2019-11-06 00:16:22 +0800

    feat: Drop extra inline styles in freeform text

--- src/renderer/issue-editor/freeform-contents.tsx
+++ src/renderer/issue-editor/freeform-contents.tsx
@@ -80,8 +80,7 @@ function adjustMenu(menu: any, selection: Selection) {
     delete _menu.table;
   }
 
-  delete _menu.marks.link;
-  delete _menu.marks.code;
+  delete _menu.marks;
   delete _menu.insert.image;
   delete _menu.blocks.code_block;
 
