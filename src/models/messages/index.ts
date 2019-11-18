e57caca557945c401f99890464133cc0a10a19a5
Anton Strogonoff <anton@strogonoff.name>
2019-11-18 12:11:56 +0800

    feat: Bring back message type title
    Useful for message editor header.

--- src/models/messages/index.ts
+++ src/models/messages/index.ts
@@ -56,6 +56,30 @@ export function isAmendment(msg: Message): msg is AmendmentMessage {
 }
 
 
+export function getMessageTypeTitle(type: MessageType): string {
+  if (type === 'approved_recommendations') {
+    return  "Approved Recommendations";
+  } else if (type === 'running_annexes') {
+    return "Lists Annexed";
+  } else if (type === 'telephone_service_2') {
+    return "Telephone Service";
+  } else if (type === 'telephone_service') {
+    return "Telephone Service";
+  } else if (type === 'callback_procedures') {
+    return "Call-back and Alt. Calling Procedures";
+  } else if (type === 'custom') {
+    return "Custom";
+  } else if (type === 'amendment') {
+    return  "Amendment";
+  } else if (type === 'service_restrictions') {
+    return "Service Restrictions";
+  } else {
+    return type;
+    //throw new Error(`Unknown message type: ${msg.type}`);
+  }
+}
+
+
 export {
   ApprovedRecommendationsMessage,
   TelephoneServiceMessage,
