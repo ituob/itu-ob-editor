0007236f22e053a0b776f65bcb6f97ae21e52380
Anton Strogonoff <anton@strogonoff.name>
2019-11-27 17:12:57 +0800

    fix: Remove erroneous import

--- src/renderer/issue-editor/message-forms/index.tsx
+++ src/renderer/issue-editor/message-forms/index.tsx
@@ -1,7 +1,7 @@
 export { MessageForm as ApprovedRecommendationsForm } from './approved_recommendations';
 export { MessageForm as RunningAnnexesForm } from './running_annexes';
 export { MessageForm as TelephoneServiceV2Form } from './telephone_service_2';
-export { MessageForm as AmendmentForm, MessageMeta as AmendmentMeta } from './amendment';
+export { MessageForm as AmendmentForm } from './amendment';
 export { MessageForm as ServiceRestrictionsForm } from './service_restrictions';
 export { MessageForm as CBProceduresForm } from './callback_procedures';
 export { MessageForm as CustomMessageForm } from './custom';
