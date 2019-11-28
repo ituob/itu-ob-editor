4444e8eaec1ee4a11e79df6cad6620c1d30d3c1b
Anton Strogonoff <anton@strogonoff.name>
2019-11-29 03:27:30 +0800

    feat(pub editor): New status icon/button in header

--- src/renderer/publication-editor/index.tsx
+++ src/renderer/publication-editor/index.tsx
@@ -2,14 +2,17 @@ import AsyncLock from 'async-lock';
 import { remote, ipcRenderer } from 'electron';
 
 import React, { useContext, useState, useEffect } from 'react';
-import { Button, FormGroup, InputGroup } from '@blueprintjs/core';
+import { FormGroup, InputGroup } from '@blueprintjs/core';
 
 import { request } from 'sse/api/renderer';
 import { PaneHeader } from 'sse/renderer/widgets/pane-header';
 import { LangConfigContext } from 'sse/localizer/renderer';
 
+import { useModified } from 'storage/renderer';
+
 import { Publication } from 'models/publications';
 import { HelpButton } from 'renderer/widgets/help-button';
+import { ObjectStorageStatus } from 'renderer/widgets/change-status';
 import {
   ObjectValidators,
   ValidationErrors,
@@ -17,7 +20,6 @@ import {
   ValidationErrorsNotice,
   GenericValidationErrorsNotice,
 } from 'renderer/form-validation';
-
 
 import * as styles from './styles.scss';
 
@@ -47,6 +49,34 @@ export const PublicationEditor: React.FC<PublicationEditorProps> = function ({ p
       if (pub) { setPublication(pub); }
     })();
   }, []);
+
+
+  /* Reload window if publication changed from outside */
+
+  function handleChanged(evt: any, data: { objIds: string[] }) {
+    // Just reload the window if our issue question changed
+    if (data.objIds.indexOf(publicationId) >= 0) {
+      remote.getCurrentWindow().reload();
+    }
+  }
+
+  useEffect(() => {
+    ipcRenderer.on('publications-changed', handleChanged);
+    return function cleanup() {
+      ipcRenderer.removeListener('publications-changed', handleChanged);
+    }
+  }, []);
+
+
+  /* Changed status mark */
+
+  const modifiedPublications = useModified().publications;
+  const [hasUncommittedChanges, setHasUncommittedChanges] = useState(false);
+
+  useEffect(() => {
+    const _hasUncommittedChanges = create || modifiedPublications.indexOf(publicationId) >= 0;
+    setHasUncommittedChanges(_hasUncommittedChanges);
+  }, [modifiedPublications]);
 
 
   /* Configure form validation */
@@ -99,7 +129,8 @@ export const PublicationEditor: React.FC<PublicationEditorProps> = function ({ p
 
   async function update(publication: Publication) {
     await pubOperationQueue.acquire('1', async () => {
-      await request<{ modified: boolean }>('publication-update', { pubId: publication.id, data: publication });
+      const updateResult = await request<{ modified: boolean }>('publication-update', { pubId: publication.id, data: publication });
+      setHasUncommittedChanges(updateResult.modified);
     });
   }
 
@@ -129,16 +160,10 @@ export const PublicationEditor: React.FC<PublicationEditorProps> = function ({ p
           align="right"
           major={true}
           actions={<HelpButton path="amend-publication/" />}>
-
-        <Button
-          disabled={canSave !== true}
-          intent="success"
-          icon="git-commit"
-          small={true}
-          onClick={commitAndClose}>Commit and Close</Button>
-
-          &ensp;
-
+        <ObjectStorageStatus
+          canSave={canSave}
+          hasUncommittedChanges={hasUncommittedChanges}
+          onCommit={commitAndClose} />
         <span>{create ? "Add" : "Edit"} service publication</span>
 
       </PaneHeader>
