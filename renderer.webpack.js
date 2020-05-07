ee5ebdc1fcee119ba03c443e18bd12dc41dc548c
Anton Strogonoff <anton@strogonoff.name>
2020-05-07 17:20:17 +0900

    chore: Add react, react-dom to externals

--- renderer.webpack.js
+++ renderer.webpack.js
@@ -3,5 +3,9 @@ const path = require('path');
 module.exports = {
   resolve: {
     modules: [path.resolve(__dirname, './src'), 'node_modules']
-  }
-}
+  },
+  externals: [
+    'react',
+    'react-dom',
+  ],
+};
