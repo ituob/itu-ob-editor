bc18a3fc3c760660d4424d5f388fcd8b34d648f7
Anton Strogonoff <anton@strogonoff.name>
2020-05-19 02:56:26 +0900

    style: Formatting

--- src/renderer/issue-editor/annex-editor.tsx
+++ src/renderer/issue-editor/annex-editor.tsx
@@ -21,7 +21,8 @@ interface AnnexEditorProps {
   onChange?: (pubId: string, updatedPosition: Date | undefined) => void
   issueId: number
 }
-export const AnnexEditor: React.FC<AnnexEditorProps> = function ({ pubId, position, onChange, issueId }) {
+export const AnnexEditor: React.FC<AnnexEditorProps> =
+function ({ pubId, position, onChange, issueId }) {
   const [editingPosition, updateEditingPosition] = useState(false);
   const pub = app.useOne<Publication, string>('publications', pubId).object;
   const latestAnnex = useLatestAnnex(pubId, issueId);
@@ -32,27 +33,41 @@ export const AnnexEditor: React.FC<AnnexEditorProps> = function ({ pubId, positi
   if (latestAnnex) {
     const date = latestAnnex.positionOn;
     if (date) {
-      previousAnnexDetails = <>Superseding position <DateStamp date={date} /> previously annexed to OB {latestAnnex.annexedTo.id}</>;
+      previousAnnexDetails = <>
+        Superseding position <DateStamp date={date} />
+        previously annexed to OB {latestAnnex.annexedTo.id}
+      </>;
     } else {
-      previousAnnexDetails = <>Superseding unspecified position previously annexed to OB {latestAnnex.annexedTo.id}</>;
+      previousAnnexDetails = <>
+        Superseding unspecified position
+        previously annexed to OB {latestAnnex.annexedTo.id}
+      </>;
     }
   } else {
-    previousAnnexDetails = <>Annexing this publication for (apparently) the first time</>;
+    previousAnnexDetails = <>
+      Annexing this publication for (apparently) the first time
+    </>;
   }
 
   let currentPosition: JSX.Element;
   if (position) {
-    currentPosition = <span className={styles.annexEditorPosition}><DateStamp date={position} /></span>;
+    currentPosition = (
+      <span className={styles.annexEditorPosition}>
+        <DateStamp date={position} />
+      </span>
+    );
   } else if (onChange && !editingPosition) {
     currentPosition = (
-      <Button
-          intent="primary"
-          onClick={() => updateEditingPosition(true)}>
+      <Button intent="primary" onClick={() => updateEditingPosition(true)}>
         Specify
       </Button>
     );
   } else {
-    currentPosition = <em className={styles.annexEditorPosition}>unspecified</em>;
+    currentPosition = (
+      <em className={styles.annexEditorPosition}>
+        unspecified
+      </em>
+    );
   }
 
 
@@ -68,10 +83,14 @@ export const AnnexEditor: React.FC<AnnexEditorProps> = function ({ pubId, positi
 
         <div className={styles.editorMeta}>
           {pub && pub.recommendation
-            ? <Text ellipsize={true}>Per <RecommendationTitle rec={pub.recommendation} /></Text>
+            ? <Text ellipsize={true}>
+                Per <RecommendationTitle rec={pub.recommendation} />
+              </Text>
             : null}
           {pubUrl !== undefined
-            ? <Text ellipsize={true}>SP resource: <a onClick={() => remote.shell.openExternal(pubUrl)}>{pubUrl}</a></Text>
+            ? <Text ellipsize={true}>
+                SP resource: <a onClick={() => remote.shell.openExternal(pubUrl)}>{pubUrl}</a>
+              </Text>
             : null}
           <div>{previousAnnexDetails}:</div>
         </div>
@@ -79,7 +98,12 @@ export const AnnexEditor: React.FC<AnnexEditorProps> = function ({ pubId, positi
 
       <FormGroup
           label={<>Position presently annexed:&ensp;{currentPosition}</>}
-          helperText={onChange && !position ? "For a dataset that’s being published multiple times, the exact position annexed should be specified here." : undefined}>
+          helperText={onChange && !position
+            ? <>
+                For a dataset that’s being published multiple times,
+                the exact position annexed currently must be specified here.
+              </>
+            : undefined}>
         {onChange && (position || editingPosition)
           ? <DatePicker
               value={position}
