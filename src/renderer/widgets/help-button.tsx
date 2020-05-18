81eb07df46f9a01efc7f8fdeb2855fdc1ceaf308
Anton Strogonoff <anton@strogonoff.name>
2020-05-19 03:25:51 +0900

    fix: Make help button work again

--- src/renderer/widgets/help-button.tsx
+++ src/renderer/widgets/help-button.tsx
@@ -3,12 +3,19 @@ import { Button } from '@blueprintjs/core';
 import { callIPC } from 'coulomb/ipc/renderer';
 
 
+const ROOT_HELP_URL = 'https://www.ituob.org/docs/';
+
+
 export const HelpButton: React.FC<{ path: string, className?: string }> = function ({ path, className }) {
   return (
     <Button
       minimal
       icon="help"
       className={className}
-      onClick={() => callIPC('open-arbitrary-window', { title: "ITU OB Editor help", url: path })} />
+      onClick={() => callIPC('open-arbitrary-window', {
+        title: "ITU OB Editor help",
+        url: `${ROOT_HELP_URL}${path}`,
+        dimensions: { width: 900 },
+      })} />
   );
 };
